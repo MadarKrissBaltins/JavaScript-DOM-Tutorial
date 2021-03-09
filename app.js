6692439b924c1b4be56dd9acef6c29497aef19bf
MadarKrissBaltins <MadarsKrissBaltins>
2021-03-09 16:17:52 +0200

    6 video commits

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-var books = document.querySelectorAll('#books-list li .name');
+const banner = document.querySelector('#page-banner');
 
-Array.from(books).forEach(function(books){
-    books.textContent +='(book title)';
-});
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeType);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
 
-const bookList = document.querySelector('#book-list');
-// bookList.innerHTML = '<h2>Books and more books...</h2>';
-bookList.innerHTML += '<p>This is how you add HTML</p>';
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);

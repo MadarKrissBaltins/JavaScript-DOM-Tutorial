612164458c01dd9b525c2d6d3a293b8e663f69ba
MadarKrissBaltins <MadarsKrissBaltins>
2021-03-16 15:36:59 +0200

    12 video commits

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+// delete books
+
 const list = document.querySelector('#book-list ul');
 
-list.addEventListener('click', function(e){
-    if(e.target.className=='delete'){
-        const li = e.target.parent
-        li.parentNode.removeChild(li)
+list.addEventListener('click', (e) => {
+        if (e.target.className == 'delete') {
+            const li = e.target.parent;
+            li.parentNode.removeChild(li);
 
-    }  
-});
+        }
+    });
 
 // add book-list
 const addForm = document.forms['add-books'];
@@ -15,4 +17,21 @@ addForm.addEventListener('submit',function(e){
     e.preventDefault();
     const value = addForm.querySelector('input[type="text"]').value;
     console.log(value);
+
+
+// create elements
+const li = documtent.createElement('li');
+const bookName = document.createElement('span');
+const bookBnt = document.createElement('span');
+
+//add content
+deleteBnt.textContent='delete';
+bookName.textContent= value;
+
+
+// appent to document
+li.appendChild(bookName);
+li.appendChild(deleteBnt);
+list.appendChild();
+
 });
